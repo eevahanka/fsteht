2a9f1d495dfa9f27a6336dc8fd79d2913e1e0ba4
Eeva <eeva.hanka@iki.fi>
2023-10-25 14:51:05 +0300

    2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
 return(
@@ -49,17 +50,20 @@ const Add = (props) => {
 }
 
 const App = (props) => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   
-
+  useEffect(() => {
+    // console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        // console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
   const handleNameChange = (event) => {
     event.preventDefault()
     // console.log("persons")
