d4832dd5da2f4286d26921e138199130a9cbdbbf
Eeva <eeva.hanka@iki.fi>
2024-02-28 16:01:34 +0200

    4.1

--- osa4/index.js
+++ osa4/index.js
@@ -1,24 +1,17 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
+  // response.send('<h1>Hello World!</h1>')
+
   Blog
     .find({})
     .then(blogs => {
@@ -36,7 +29,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
