6f6c4df5a767d500606d5032a6b5dd755b67d7d7
Eeva <eeva.hanka@iki.fi>
2024-02-29 15:10:16 +0200

    its works

--- osa4/index.js
+++ osa4/index.js
@@ -1,37 +1,7 @@
-require('dotenv').config()
+const app = require('./app.js') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-
-const Blog = require('./models/blog')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  // response.send('<h1>Hello World!</h1>')
-
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
