3a0f8a5eeacc100aaea6c98cadb6c0e1e813eeb3
Eeva <eeva.hanka@iki.fi>
2024-02-29 15:15:49 +0200

    stable

--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -1,32 +1,20 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI
 
-console.log('connecting ', url)
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-  const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-
-  blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-
-  module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model('Blog', blogSchema)
