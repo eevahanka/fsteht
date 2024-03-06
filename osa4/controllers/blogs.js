c915f17f52b4b9774e960e7300d2ce7f15d54bb8
Eeva <eeva.hanka@iki.fi>
2024-03-06 14:09:21 +0200

    4.10

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -6,7 +6,7 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
  
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const body = request.body
     const blog = new Blog({
         title: body.title,
@@ -14,11 +14,8 @@ blogsRouter.post('/', (request, response) => {
         url: body.url,
         likes: body.likes
     })
-    blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
-        })
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
     })
 
 module.exports = blogsRouter
