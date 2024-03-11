df8a2201c8505c22a1d9646103ca18863f77ba39
Eeva <eeva.hanka@iki.fi>
2024-03-11 13:09:08 +0200

    4.19

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+      return authorization.replace('Bearer ', '')
+    }
+    return null
+  }
 
 blogsRouter.get('/', async (request, response) => { 
   const blogs = await Blog.find({}).populate('user', {name:1, username:1})
@@ -15,7 +24,11 @@ blogsRouter.post('/', async (request, response) => {
     else if (!body.url) {response.status(400).end()} 
     else {
         // const user = await User.findById(body.userId)
-        const user = await User.findOne({username:'Test'})
+        const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+        if (!decodedToken.id) {
+            return response.status(401).json({ error: 'token invalid' })
+        }
+        const user = await User.findById(decodedToken.id)
         console.log(user.id)
         const blog = new Blog({
             title: body.title,
