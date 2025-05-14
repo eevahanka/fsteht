8f33c86ee6405754951f79654d6ce465d35cd66f
Eeva <eeva.hanka@iki.fi>
2025-05-14 14:10:25 +0300

    5.14

---
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
+
+
+test('renders content', () => {
+  const blog = {
+    title: 'testi',
+    author: 'testaaja',
+    url: 'www.testi.fi',
+    likes: 0
+  }
+
+  render(<Blog blog={blog} />)
+
+  const element = screen.getByText('testi – testaaja')
+  expect(element).toBeDefined()
+})
+
+test('likes rendered when opened', async () => {
+  const blog = {
+    title: 'testi',
+    author: 'testaaja',
+    url: 'www.testi.fi',
+    likes: 0
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('show')
+  await user.click(button)
+  const element = screen.getByText('0 likes')
+  expect(element).toBeDefined()
+})
