ddc671e0879ffa838ad678c5ac09172769301e34
Eeva <eeva.hanka@iki.fi>
2025-05-14 14:28:02 +0300

    5.15'

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -11,7 +11,7 @@ test('renders content', () => {
     likes: 0
   }
 
-  render(<Blog blog={blog} />)
+  render(<Blog blog={blog}/>)
 
   const element = screen.getByText('testi – testaaja')
   expect(element).toBeDefined()
@@ -25,7 +25,6 @@ test('likes rendered when opened', async () => {
     likes: 0
   }
 
-  const mockHandler = vi.fn()
 
   render(<Blog blog={blog} />)
 
@@ -35,3 +34,25 @@ test('likes rendered when opened', async () => {
   const element = screen.getByText('0 likes')
   expect(element).toBeDefined()
 })
+
+test('like clicked twise', async () => {
+  const blog = {
+    title: 'testi',
+    author: 'testaaja',
+    url: 'www.testi.fi',
+    likes: 0
+  }
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} handlelike={mockHandler}/>)
+
+  const user = userEvent.setup()
+  const showbutton = screen.getByText('show')
+  await user.click(showbutton)
+  const likebutton = screen.getByText('like')
+  await user.click(likebutton)
+  await user.click(likebutton)
+  
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
+
