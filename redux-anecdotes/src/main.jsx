c11c5402b6ac1c9085836b9fe9a3729caefdf52d
Eeva <eeva.hanka@iki.fi>
2025-05-30 17:06:30 +0300

    teen 6.10 mut se on viel keske, mut tällene kiva kesken refactorointii kommitti

--- redux-anecdotes/src/main.jsx
+++ redux-anecdotes/src/main.jsx
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
 import aneReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  anes: aneReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    anes: aneReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,4 @@
+import { createSlice } from '@reduxjs/toolkit'
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
